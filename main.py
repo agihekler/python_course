b936ce97bc5de566b4864c20a6aaa3f3e8267659
Agi Hekler <agnes.hekler@gmail.com>
2020-11-11 14:14:07 -0500

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import circle
+
+def main(): 
+  r = 3
+  
+if __name__ == "__main__": 
+  main()
+print ("Circle with r = 3")
+print ("-----------------")
+import math
+r = 3
+c = 2*math.pi*r
+a = math.pi*r**2
+print ("Circumference is", c)
+print ("Area is", a)
+
